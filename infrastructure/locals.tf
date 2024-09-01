72f1b5b99a622b0bacdf13873164098f29df0d14
Pascal Frenz <5677521+PascalFrenz@users.noreply.github.com>
2024-09-01 10:13:55 +0200

    add terraform linter workflow & adjust locals.tf

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -5,6 +5,6 @@ locals {
   alb_name         = "${var.stage}-${local.application_name}"
   codedeploy_name  = "${var.stage}-${local.application_name}"
 
-  example_service_docker_image = "registry.gitlab.com/pascal-frenz/oss/aws-ecs-codedeploy-terraform/webserver:1.0.0"
+  example_service_docker_image = "ghcr.io/pascalfrenz/aws-ecs-codedeploy-terraform/webserver:latest"
   example_service_port         = 8080
 }
